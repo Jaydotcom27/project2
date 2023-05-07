94e1d9706c2f24b2da948bd0afd087c08486e4dc
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-05-07 12:52:35 -0400

    wrapped with hive support

--- task1/task1.py
+++ task1/task1.py
@@ -6,56 +6,50 @@ import pyspark.sql.functions as F
 import pyspark.sql.types as T
 import sys
 
-
 def main():
-    # create a spark session
-    spark = SparkSession.builder.appName("task1").getOrCreate()
-
-    # read training and test data from CSV files
+    spark = SparkSession.builder.appName("Toxic Comment Classification").enableHiveSupport().getOrCreate()
     train_data = spark.read.csv(sys.argv[1], header=True)
     test_data = spark.read.csv(sys.argv[2], header=True)
 
-    # list of output columns
-    out_cols = [i for i in train_data.columns if i not in ["id", "comment_text"]]
+    # columns to be outputted
+    output_columns = [i for i in train_data.columns if i not in ["id", "comment_text"]]
 
-    # tokenize the "comment_text" column
+    # Breaking down the "comment_text" column
     tokenizer = Tokenizer(inputCol="comment_text", outputCol="words")
-    word = tokenizer.transform(train_data)
     train_data = train_data.withColumn("toxic", col("toxic").cast("int")).withColumn("severe_toxic", col("severe_toxic").cast("int")).withColumn("obscene", col("obscene").cast("int")).withColumn("threat", col("threat").cast("int")).withColumn("insult", col("insult").cast("int")).withColumn("identity_hate", col("identity_hate").cast("int"))
     train_data = train_data.na.drop("any")
-
     words_data = tokenizer.transform(train_data)
-    # remove any rows that contain null values
+
+    # removing incomplete rows -> these are not allowed when using the IDF transformer so it'll break if not removed
     words_data = words_data.na.drop("any")
 
-    # apply hashing trick and IDF transformation to tokenized text
+    # apply hashing trick and IDF transformation to tokenized text, this is similar to embedding 
     hashing_tf = HashingTF(inputCol="words", outputCol = "R")
     hash_tf = hashing_tf.transform(words_data)
     idf = IDF(inputCol = "R", outputCol = "features")
     idf_model = idf.fit(hash_tf)
     tf_idf = idf_model.transform(hash_tf)
 
-    # preprocess test data
+    # preprocessing our test data
     test_data = test_data.filter(col("comment_text") != '"').withColumn('UID', monotonically_increasing_id())
     test_words = tokenizer.transform(test_data)
     test_hash_tf = hashing_tf.transform(test_words)
     test_tf_idf = idf_model.transform(test_hash_tf)
-    test_res = test_data.select("UID")
+    test_results = test_data.select("UID")
 
-    # train logistic regression model and make predictions on test data
+    # train logistic regression model and predict
     reg_param = 0.1
-    for the_column in out_cols:
+    for the_column in output_columns:
         lr = LogisticRegression(featuresCol="features", labelCol=the_column, regParam=reg_param)
         lr_model = lr.fit(tf_idf.limit(4000))
         res_test = lr_model.transform(test_tf_idf)
         extract_prob = F.udf(lambda x: float(x[1]), T.FloatType())
         res_test = res_test.withColumn("proba" + '_' + the_column, extract_prob("probability")).withColumn("pred" + '_' + the_column, col('prediction'))
         res_test = res_test.select(res_test["UID"], res_test["proba" + '_' + the_column], res_test["pred" + '_' + the_column])
-        test_res = test_res.join(res_test, on=["UID"])
+        test_results = test_results.join(res_test, on=["UID"])
 
-    # display sample predictions
-    test_res.sample(False, 0.4).show()
-
+    # print predictions
+    test_results.sample(False, 0.4).show()
 
 if __name__ == "__main__":
     main()
