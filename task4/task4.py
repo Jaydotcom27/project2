f3235dc981cc3952b791b5e9dea8ae75fc8698cf
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-05-08 13:02:41 -0400

    Updated to RF and DT

--- task4/task4.py
+++ task4/task4.py
@@ -1,141 +1,23 @@
 from __future__ import print_function
 import sys
-from operator import add
-
-
-
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as sql_functions
 from pyspark.ml.feature import VectorAssembler, OneHotEncoder, StringIndexer
 from pyspark.ml.classification import DecisionTreeClassifier, RandomForestClassifier
-from pyspark.mllib.stat import Statistics
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: Logistic Regression Adult Dataset <file>", file=sys.stderr)
-        sys.exit(-1)
-
-    spark = SparkSession\
-        .builder\
-        .appName("task3")\
-        .getOrCreate()
-    
-    train_df = spark.read.csv(sys.argv[1], header = False, inferSchema = True)
-
-    column_names = ["age", "workClass", "fnlwgt", "education", "educationNum", "maritalStatus", "occupation", "relationship", "race", "sex", "capitalGain", "capitalLoss", "hoursPerWeek", "nativeCountry", "Income"]
+def main(spark, input_path): 
+    train_df = spark.read.csv(input_path, header = False, inferSchema = True)
+    column_names = ["age", "workClass", "fnlwgt", "education", "educationNum", "maritalStatus", "occupation", "relationship", "race", "sex", "capitalGain", "capitalLoss", "hoursPerWeek", "nativeCountry", "Labels"]
     train_df = train_df.toDF(*column_names)
-
     test_df = spark.sparkContext.textFile(sys.argv[2]).map(lambda line : line.split(","))
     test_df = test_df.filter( lambda line : len(line) >2 )
     test_df = test_df.toDF()
     test_df = test_df.toDF(*column_names)
+    res_df = train_df.union(test_df)
+    final_df = preProcess(res_df)
 
-    df_runing = train_df.union(test_df)
-
-
-    def preprocessingdata(spark_df):
-
-        column_names_new=spark_df.columns
-        for i in column_names_new:
-            spark_df=spark_df.withColumn(i,sql_functions.trim(sql_functions.col(i)))
-        
-        #The dataset has a couple of values with value ?, replace with null values if applicable        
-        for i in column_names_new:
-            spark_df=spark_df.withColumn(i,sql_functions.when(sql_functions.col(i)=="?", None).otherwise(sql_functions.col(i)))
-
-        #drop null values
-        spark_df1a=spark_df.na.drop("any")    
-
-        #Change Income class variable to 0 and 1
-        spark_df1=spark_df1a.withColumn("Income", sql_functions.when((sql_functions.col("Income") == "<=50K") | (sql_functions.col("Income") == "<=50K."), 0).otherwise(sql_functions.col("Income"))).withColumn("Income", sql_functions.when((sql_functions.col("Income") == ">50K")| (sql_functions.col("Income") == ">50K."), 1).otherwise(sql_functions.col("Income")))
-
-
-        #once I transform the categorical data to numeric data, transform capital gain and loss to log and scale all the numeric feaures. then perform titus technique to stay with most important features
-        #already have the same feature in numeric
-        spark_df1=spark_df1.drop("education")
-
-        #Tranform categorical string data to numeric
-        categorical_features=["workClass","maritalStatus","occupation", "relationship","race", "sex","nativeCountry"]
-        categorical_index=StringIndexer(inputCols=categorical_features, 
-                                        outputCols= ["workClass_num","maritalStatus_num","occupation_num", "relationship_num","race_num", "sex_num","nativeCountry_num"])
-        spark_df2=categorical_index.fit(spark_df1).transform(spark_df1)
-
-        for i in categorical_features:
-            spark_df2=spark_df2.drop(i)
-
-        new_columns =["age", "fnlwgt", "educationNum","capitalGain", "capitalLoss", "hoursPerWeek", "Income", 
-                      "workClass", "maritalStatus", "occupation", "relationship", "race", "sex", "nativeCountry"]
-        spark_df2=spark_df2.toDF(*new_columns)
-
-        #Log transform capital gain and capital loss adn fnlwgt.
-        skewed = ['capitalGain', 'capitalLoss','fnlwgt']
-        #scale the remaining numeric features
-        spark_df2 = spark_df2.withColumn(skewed[0], sql_functions.log1p(sql_functions.col(skewed[0]))) \
-                                      .withColumn(skewed[1], sql_functions.log1p(sql_functions.col(skewed[1]))) \
-                                      .withColumn(skewed[2], sql_functions.log1p(sql_functions.col(skewed[2])))
-        
-        for i in spark_df2.columns:
-            if i!="Income":
-                spark_df2=spark_df2.withColumn(i, sql_functions.col(i).cast("float"))
-
-        spark_df3=spark_df2.withColumn("Income",sql_functions.col("Income").cast("int"))
-
-        #Link the new categorical features to numerical values
-        encoder=OneHotEncoder(inputCols=categorical_features, 
-                              outputCols=["workClass_num","maritalStatus_num","occupation_num", "relationship_num","race_num", "sex_num","nativeCountry_num"])
-
-        #Incorporate new features to train dataset
-        X_train=encoder.fit(spark_df3).transform(spark_df3)
-        for i in categorical_features:
-            X_train=X_train.drop(i)
-        
-
-        #Perform Pearson correlation test to stay with most important features
-        col_names = X_train.columns
-
-        # Initialize lists to store the results
-        param = []
-        correlation = []
-        abs_cor = []
-        for c in col_names:
-            if c != "income":
-                corr = abs(Statistics.corr(X_train.select('Income', c).rdd.map(lambda x: (float(x[0]), float(x[1]))), method="pearson"))
-                param.append(c)
-                correlation.append(corr[0][1])
-                abs_cor.append(abs(corr[0][1]))
-
-                
-        # Create a DataFrame from the results
-        param_df = X_train.createDataFrame(zip(correlation, param, abs_cor), ["correlation", "parameter", "abs_cor"])
-
-        # Sort the DataFrame by absolute correlation in descending order
-        param_df = param_df.orderBy(sql_functions("abs_cor").desc())
-
-        # Set the 'parameter' column as the index
-        param_df = param_df.withColumnRenamed("parameter", "index").drop("abs_cor").drop("correlation")
-
-        best_features = param_df.select("index").limit(8).rdd.flatMap(lambda x: x).collect()
-
-
-        #top 8 best features
-        print("Top features to include in model:",best_features) 
-
-        #Remove the class variable
-        columns=best_features.columns
-        train_columns=list()
-        for i in columns:
-            if i!="Income":
-                train_columns.append(i)
-
-        # Create a VectorAssembler to combine the features into a single vector column
-        assembler = VectorAssembler(inputCols=train_columns, outputCol="features")
-        final_X_train = assembler.transform(X_train)
-
-        return final_X_train
-    
-    final_df = preprocessingdata(df_runing)
-    test_train_split = final_df.randomSplit([0.8,0.2],47)
+    # Random 80/20 data split
+    test_train_split = final_df.randomSplit([0.8,0.2],47) # <- using same seed
     X_train = test_train_split[0]
     X_test = test_train_split[1]
 
@@ -178,4 +60,60 @@ if __name__ == "__main__":
 
     spark.stop()
 
+def preProcess(spark_df):
+    # Trim all the columns and drop not used
+    c_names = spark_df.columns
+    for colname in c_names:
+        spark_df = spark_df.withColumn(colname, F.trim(F.col(colname)))
+    spark_df = spark_df.drop("fnlwgt", "education")
 
+    # Replace all the "?" with None and drop all rows with missing values
+    for colname in spark_df.columns:
+        spark_df = spark_df.withColumn(colname, F.when(F.col(colname) == "?", None).otherwise(F.col(colname)))
+    spark_df = spark_df.na.drop("any")
+    cat_cols = ["workClass", "maritalStatus", "occupation", "relationship", "race", "sex", "nativeCountry"]
+    
+    # Index all the categorical features and drop them
+    string_indexer = StringIndexer(inputCols=cat_cols, outputCols=[f"{col}_index" for col in cat_cols])
+    spark_df = string_indexer.fit(spark_df).transform(spark_df)
+    spark_df = spark_df.drop(*cat_cols)
+
+    # Rename the columns for convenience
+    spark_df = spark_df.toDF("age", "educationNum","capitalGain", "capitalLoss", "hoursPerWeek", "Labels", "workClass", "maritalStatus", "occupation", "relationship", "race", "sex", "nativeCountry")
+    spark_df = spark_df.drop("capitalGain", "capitalLoss")
+
+    # Convert all float columns to float
+    for colname in spark_df.columns:
+        if colname in ["Labels", "workClass", "maritalStatus", "occupation", "relationship", "race", "sex", "nativeCountry"]:
+            continue
+        spark_df = spark_df.withColumn(colname, F.col(colname).cast("float"))
+
+    # Modify the "Labels" column to suit the ML algorithms
+    spark_df = spark_df.withColumn("Labels", F.when(F.col("Labels").isin(["<=50K", "<=50K."]), 0).otherwise(F.col("Labels")))
+    spark_df = spark_df.withColumn("Labels", F.when(F.col("Labels").isin([">50K", ">50K."]), 1).otherwise(F.col("Labels")))
+    spark_df = spark_df.withColumn("Labels", F.col("Labels").cast("int"))
+
+    # One-hot encode all the categorical features
+    encoder = OneHotEncoder(inputCols=["workClass", "maritalStatus", "occupation", "relationship", "race", "sex", "nativeCountry"], 
+                            outputCols=["wc_vec", "ms_vec", "occ_vec", "rel_vec", "race_vec", "sex_vec", "natCou_vec"])
+    spark_df = encoder.fit(spark_df).transform(spark_df)
+    spark_df = spark_df.drop("workClass", "maritalStatus", "occupation", "relationship", "race", "sex", "nativeCountry")
+
+    # All columns except "Labels" are used as training features
+    assembler = VectorAssembler(inputCols=[col for col in spark_df.columns if col != "Labels"],
+                                outputCol="features")
+    spark_df = assembler.transform(spark_df)
+
+    return spark_df
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Logistic Regression Adult Dataset", file=sys.stderr)
+        sys.exit(-1)
+
+    spark = SparkSession.builder.appName("RF and DT on Census Income Data").enableHiveSupport().getOrCreate()
+    input_path = sys.argv[1]
+    main(spark, input_path)
+
+
+    
